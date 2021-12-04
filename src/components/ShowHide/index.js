3e49b9cb819233c9d7bde8e817a3420ec22f4dab
sdenvs <sdenvs@gmail.com>
2021-12-04 18:11:53 +0530

    changed

--- src/components/ShowHide/index.js
+++ src/components/ShowHide/index.js
@@ -1,1 +1,51 @@
 // Write your code here
+
+import './index.css'
+import {Component} from 'react'
+
+class ShowHide extends Component {
+  state = {showFirst: true, showLast: true}
+
+  showHideFirst = () => {
+    const {showFirst} = this.state
+    if (showFirst) {
+      this.setState(prevObj => ({showFirst: false}))
+    } else {
+      this.setState(prevObj => ({showFirst: true}))
+    }
+  }
+
+  showHideLast = () => {
+    const {showLast} = this.state
+    if (showLast) {
+      this.setState(prevObj => ({showLast: false}))
+    } else {
+      this.setState(prevObj => ({showLast: true}))
+    }
+  }
+
+  render() {
+    const {showFirst, showLast} = this.state
+    return (
+      <div className="background">
+        <h1 className="heading">Show/Hide</h1>
+        <div className="card">
+          <div>
+            <button onClick={this.showHideFirst} className="button">
+              Show/Hide Firstname
+            </button>
+            {showFirst && <h1 className="nameBox">Joe</h1>}
+          </div>
+          <div>
+            <button onClick={this.showHideLast} className="button">
+              Show/Hide Lastname
+            </button>
+            {showLast && <h1 className="nameBox">Jonas</h1>}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ShowHide
